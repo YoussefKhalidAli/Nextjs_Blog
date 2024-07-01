8868b36358f4b20e3e7e7dbdce10e2e007b70b9c
YoussefKhaildAli <yossofwd@gmail.com>
2024-07-01 10:21:06 +0300

    Navigation Bar

---
+++ src/components/CreatePostNavigator/CreatePostNavigator.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css";
+
+export default function App() {
+  return (
+    <div className={styles.createPostNavigator}>
+      <h2>Have something share?</h2>
+      <a>Create Post</a>
+    </div>
+  );
+}
---
+++ src/components/navigationBar/NavigationBar.tsx
@@ -0,0 +1,9 @@
+import styles from "./NavigationBar.module.css";
+
+export default function NavigationBar() {
+  return (
+    <div className={styles.navBar}>
+      <h1>Slash</h1>
+    </div>
+  );
+}
